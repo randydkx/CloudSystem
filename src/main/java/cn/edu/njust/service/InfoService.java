24e28922c21a8d57e694bb38e836b8e705ceba4b
studio-tyx <1341913265@qq.com>
2021-07-15 21:10:58 +0800

    增加了一些前后端服务接口
    1.增加了InfoService：获取资源使用量及集群相关信息
    2.增加了LogService：获取日志信息

---
+++ src/main/java/cn/edu/njust/service/InfoService.java
@@ -0,0 +1,29 @@
+package cn.edu.njust.service;
+
+import cn.edu.njust.entity.*;
+
+import java.util.List;
+
+/**
+ * @author TYX
+ * @name InfoService
+ * @description 获取当前的资源使用量以及有关集群容器、Pod、Node的信息
+ * @createTime 2021/7/15 21:01
+ **/
+public class InfoService {
+    public Usage getUsage() {
+        return null;
+    }
+
+    public List<ContainerInfo> getContainerInfo() {
+        return null;
+    }
+
+    public List<PodInfo> getPodInfo() {
+        return null;
+    }
+
+    public List<NodeInfo> getNodeInfo() {
+        return null;
+    }
+}
