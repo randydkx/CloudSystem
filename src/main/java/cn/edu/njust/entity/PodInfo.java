dae914635cbe0b68666817bb2d9e8a6b436c1f5f
studio-tyx <1341913265@qq.com>
2021-07-30 18:03:06 +0800

    实现了getPodInfo函数
    1.实现了InfoService接口中返回Pod信息的函数
    2.Pod AGE字段对应的接口函数尚未找到，暂时使用LastTransitionTime字段替代

--- src/main/java/cn/edu/njust/entity/PodInfo.java
+++ src/main/java/cn/edu/njust/entity/PodInfo.java
@@ -13,5 +13,24 @@ public class PodInfo {
     private String name;
     private String status;
     private String age;
-    private List<ContainerInfo> containers; //理论上只有一个容器
+    private String containers; //理论上只有一个容器
+
+    public PodInfo(String namespace, String name, String status, String age, String containers) {
+        this.namespace = namespace;
+        this.name = name;
+        this.status = status;
+        this.age = age;
+        this.containers = containers;
+    }
+
+    @Override
+    public String toString() {
+        return "PodInfo{" +
+                "namespace='" + namespace + '\'' +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", age='" + age + '\'' +
+                ", containers='" + containers + '\'' +
+                '}';
+    }
 }
--- src/main/java/cn/edu/njust/service/InfoService.java
+++ src/main/java/cn/edu/njust/service/InfoService.java
@@ -1,7 +1,21 @@
 package cn.edu.njust.service;
 
 import cn.edu.njust.entity.*;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1ContainerStatus;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodCondition;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.KubeConfig;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,11 +33,43 @@ public class InfoService {
         return null;
     }
 
-    public List<PodInfo> getPodInfo() {
-        return null;
+    public List<PodInfo> getPodInfo(String kubeConfigPath) throws IOException, ApiException {
+        ApiClient client=null;
+        List<PodInfo> res=new ArrayList<PodInfo>();
+        PodInfo pod;
+//        String kubeConfigPath = "C:\\config";   //指定config
+        client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        for (V1Pod item : list.getItems()) {
+            String podName=item.getMetadata().getName();
+            String podNamespace=item.getMetadata().getNamespace();
+            String podStatus=item.getStatus().getPhase();
+//            System.out.println("item.getStatus():"+item.getStatus());
+//            String podAge=item.getStatus().getStartTime().toString();
+            List<V1PodCondition> conditions = item.getStatus().getConditions();
+            String podAge=conditions.get(conditions.size()-1).getLastTransitionTime().toString();
+            List<V1ContainerStatus> containerStatuses = item.getStatus().getContainerStatuses();
+            String containerName="null";
+            if(containerStatuses!=null) containerName=containerStatuses.get(containerStatuses.size()-1).getName();
+//            System.out.println("containerStatuses.get(last):"+containerStatuses);
+            pod=new PodInfo(podNamespace,podName,podStatus,podAge,containerName);
+            res.add(pod);
+        }
+        return res;
     }
 
     public List<NodeInfo> getNodeInfo() {
         return null;
+    }
+
+    public static void main(String[] args) throws IOException, ApiException {
+        InfoService infoService=new InfoService();
+        List<PodInfo> podInfo = infoService.getPodInfo("C:\\config");
+        for(PodInfo pod:podInfo){
+            System.out.println(pod);
+        }
     }
 }
