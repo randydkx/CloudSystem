c9ca2e5643f8192f9681fffad28a9a4a424514f2
studio-tyx <1341913265@qq.com>
2021-08-20 21:41:34 +0800

    增加了与Linux交互的工具类

---
+++ src/main/java/cn/edu/njust/utils/linuxshell/LinuxDataBase.java
@@ -0,0 +1,40 @@
+package cn.edu.njust.utils.linuxshell;
+
+public class LinuxDataBase {
+    private String username;
+    private String password;
+    private String url;
+    private String port;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+}
